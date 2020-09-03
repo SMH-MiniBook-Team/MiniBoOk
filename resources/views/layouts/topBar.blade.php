accee4287e716e4aa0def52f7983598f47decd43
Mouna-pixel <mounaelyounssi@gmail.com>
2020-09-03 14:21:13 +0100

    Search for people backend , add friends , receive friend request and accepting them .

--- resources/views/layouts/topBar.blade.php
+++ resources/views/layouts/topBar.blade.php
@@ -13,9 +13,10 @@
     </button>
 
     <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" role="search" action="#">
+    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" role="search" action="{{route('search.results')}}
+">
       <div class="input-group">
-        <input type="text"  class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+        <input type="text" name="query" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
         <div class="input-group-append">
           <button class="btn" type="submit"
            style="
